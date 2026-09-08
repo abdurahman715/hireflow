0c423643adfbb8095ae8fd4b73d40156f1b2b2aa
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-09-08 13:11:47 +0530

    chore:initialize auth service

---
+++ services/auth/src/index.ts
@@ -0,0 +1,10 @@
+import app from "./app.js";
+import dontenv from "dotenv";
+
+dontenv.config();
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Auth service is running on http://localhost:${process.env.PORT}`,
+  );
+});
