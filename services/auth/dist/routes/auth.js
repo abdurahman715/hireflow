3f37f15b25032cce5f74fe305a41a86c43b9979e
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-09-10 11:05:21 +0530

    feat(auth): implement user login with JWT token

--- services/auth/dist/routes/auth.js
+++ services/auth/dist/routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { registerUser } from "../controllers/auth.js";
+import { loignUser, registerUser } from "../controllers/auth.js";
 import uploadFile from "../middleware/multer.js";
 const router = express.Router();
 router.post("/register", uploadFile, registerUser);
+router.post("/login", loignUser);
 export default router;
--- services/auth/src/routes/auth.ts
+++ services/auth/src/routes/auth.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { registerUser } from "../controllers/auth.js";
+import { loignUser, registerUser } from "../controllers/auth.js";
 import uploadFile from "../middleware/multer.js";
 
 const router = express.Router();
 router.post("/register", uploadFile, registerUser);
+router.post("/login", loignUser);
 export default router;
